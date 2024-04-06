0893665e1feda330ec328e75ad70980f76647e42
YurHar <harutyunyan702@gmail.com>
2024-04-06 18:03:34 +0400

    changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
     };
 
     return (
-        <div className={`flex flex-col h-screen ${darkMode ? 'bg-slate-800 text-white' : 'bg-light'}`}>
+        <div className={`flex flex-col h-screen ${darkMode && 'bg-slate-800 text-white'}`}>
             <nav className={`bg-slate-200 ${darkMode && 'bg-slate-500'}`}>
                 <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
