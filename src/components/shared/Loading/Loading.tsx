cb4fa394ad78955a14c185d35473bbb760ea5441
yuri.harutyunyan <yuri.harutyunyan@nork.am>
2024-05-28 19:10:15 +0400

    change style

--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -2,9 +2,11 @@ import "./Loading.css";
 
 const Loading = () => {
   return (
-    <div className="loadingio-spinner-rolling-nq4q5u6dq7r">
-      <div className="ldio-x2uulkbinbj">
-        <div></div>
+    <div className="flex justify-center">
+      <div className="loadingio-spinner-rolling-nq4q5u6dq7r">
+        <div className="ldio-x2uulkbinbj">
+          <div></div>
+        </div>
       </div>
     </div>
   );
--- src/features/users/components/UserList.tsx
+++ src/features/users/components/UserList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchUsersAsync, setPage } from "../slices/userSlice";
